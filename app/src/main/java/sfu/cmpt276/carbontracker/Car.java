0c037fb15234d1161b88a1add60f8138b89d17c2
ckc21 <ckc21@sfu.ca>
2017-03-02 13:48:55 -0800

    added more member variables to Car class formatted CSV

--- app/src/main/java/sfu/cmpt276/carbontracker/Car.java
+++ app/src/main/java/sfu/cmpt276/carbontracker/Car.java
@@ -1,7 +1,7 @@
 package sfu.cmpt276.carbontracker;
 
 /**
- * Created by Medipack on 2017-03-01.
+ * Class to hold profile data for cars
  */
 
 public class Car {
@@ -9,24 +9,32 @@ public class Car {
     private String nickname;
     private String model;
     private String make;
+    private String fuelType;
+    private String transmission;
     private int year;
-    private double efficiency;
+    private int cityCO2;
+    private int hwyCO2;
+    private int engineDispl;
 
     //Constructor
     public Car(){
         nickname = Default;
         model = Default;
         make = Default;
+        transmission = Default;
         year = 0;
-        efficiency = 0;
+        cityCO2 = 0;
+        hwyCO2 = 0;
+        engineDispl = 0;
     }
 
-    public Car(String nickname, String model, String make, int year, double efficiency){
+    public Car(String nickname, String model, String make, int year, int cityCO2, int hwyCO2){
         this.nickname = nickname;
         this.model = model;
         this.make = make;
         this.year = year;
-        this.efficiency = efficiency;
+        this.cityCO2 = cityCO2;
+        this.hwyCO2 = hwyCO2;
     }
 
     //Getter
@@ -43,8 +51,24 @@ public class Car {
         return year;
     }
 
-    public double getEfficiency() {
-        return efficiency;
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public int getCityCO2() {
+        return cityCO2;
+    }
+
+    public int getHwyCO2() {
+        return hwyCO2;
+    }
+
+    public int getEngineDispl() {
+        return engineDispl;
     }
 
     //Setter
@@ -60,8 +84,24 @@ public class Car {
         this.year = year;
     }
 
-    public void setEfficiency(double efficiency) {
-        this.efficiency = efficiency;
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public void setTransmission(String transmission) {
+        this.transmission = transmission;
+    }
+
+    public void setCityCO2(int cityCO2) {
+        this.cityCO2 = cityCO2;
+    }
+
+    public void setHwyCO2(int hwyCO2) {
+        this.hwyCO2 = hwyCO2;
+    }
+
+    public void setEngineDispl(int engineDispl) {
+        this.engineDispl = engineDispl;
     }
 }
 
--- app/src/main/java/sfu/cmpt276/carbontracker/carDirectory.java
+++ app/src/main/java/sfu/cmpt276/carbontracker/carDirectory.java
@@ -1,11 +1,24 @@
 package sfu.cmpt276.carbontracker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Class to keep a record of all the car data read from the CSV
  */
 
-public class carDirectory {
+public class carDirectory extends Car{
 
+    private List mainList = new ArrayList<Car>();
 
+    carDirectory(){
+
+    }
+    public Car getCar(int index){
+       return (Car) mainList.get(index);
+    }
+    public void addCar(Car newCar){
+        mainList.add(newCar);
+    }
 
 }
