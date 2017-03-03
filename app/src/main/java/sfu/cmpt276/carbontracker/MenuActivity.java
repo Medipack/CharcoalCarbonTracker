b766a4f370a6b2cf970bb9fdd2a03545805a96a3
jarinyah <jarinyah@sfu.ca>
2017-03-03 12:19:43 -0800

    Setup main menu buttons to launch activities
    launches transportation mode activity (must be merged with separate branch for ui)
    launches carbon footprint activity (ui not yet designed)
    refactored naming of menu activity to match origin

---
+++ app/src/main/java/sfu/cmpt276/carbontracker/MenuActivity.java
@@ -0,0 +1,43 @@
+package sfu.cmpt276.carbontracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        setupNewJourneyBtn();
+        setupCarbonTotalsBtn();
+    }
+
+    private void setupNewJourneyBtn()
+    {
+        Button newJourneyBtn = (Button)findViewById(R.id.createJourneyBtn);
+        newJourneyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, TransportationModeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void setupCarbonTotalsBtn()
+    {
+        Button newJourneyBtn = (Button)findViewById(R.id.createJourneyBtn);
+        newJourneyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, CarbonFootprintActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
