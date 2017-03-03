05128077297c65bfb8f0de1f437ec09503392e21
Tim Robertson <timr@sfu.ca>
2017-03-03 02:40:46 -0800

    Add callback to select / longpress on listview items
    All actions are logged
    Small refactor

--- app/src/main/java/sfu/cmpt276/carbontracker/TransportationModeActivity.java
+++ app/src/main/java/sfu/cmpt276/carbontracker/TransportationModeActivity.java
@@ -7,15 +7,15 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
-
-import junit.framework.Test;
+import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.List;
+
 
 class TestCarClass {
     String nickname;
@@ -31,8 +31,6 @@ class TestCarClass {
     }
 }
 
-
-
 public class TransportationModeActivity extends AppCompatActivity {
 
     private final String TAG = "TransportationActivity";
@@ -44,16 +42,18 @@ public class TransportationModeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transportation_mode);
 
-
         setUpAddVehicleButton();
+        setUpCarListView();
+        registerListViewClickCallback();
 
         addTestVehicleToArray();
     }
 
+
     private void addTestVehicleToArray() {
         carArrayList.add(new TestCarClass("My fav car", "Lamborghini", "Diablo", 1999));
         carArrayList.add(new TestCarClass("The fun car", "Porsche", "911", 2017));
-        updateCarListView();
+        carArrayList.add(new TestCarClass("The Ancient One", "Honda", "Civic", 1985));
     }
 
     private class CarListAdapter extends ArrayAdapter<TestCarClass> {
@@ -79,10 +79,9 @@ public class TransportationModeActivity extends AppCompatActivity {
 
             return itemView;
         }
-
     }
 
-    private void updateCarListView() {
+    private void setUpCarListView() {
         ArrayAdapter<TestCarClass> carListAdapter = new CarListAdapter(TransportationModeActivity.this);
         ListView carList = (ListView) findViewById(R.id.carListView);
         carList.setAdapter(carListAdapter);
@@ -95,6 +94,41 @@ public class TransportationModeActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Log.i(TAG, "Add Vehicle Button clicked");
+            }
+        });
+    }
+
+    private void registerListViewClickCallback() {
+        ListView list = (ListView) findViewById(R.id.carListView);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                // User has selected a vehicle
+                TestCarClass car = carArrayList.get(i);
+                Log.i(TAG, "User selected a vehicle");
+
+                Toast.makeText(TransportationModeActivity.this,
+                        "Selected " + car.make + " " + car.model,
+                        Toast.LENGTH_SHORT).show();
+
+                // todo go to route activity
+            }
+        });
+
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                // User has long pressed to edit a vehicle
+                TestCarClass car = carArrayList.get(i);
+                Log.i(TAG, "User long pressed on a vehicle");
+
+                Toast.makeText(TransportationModeActivity.this,
+                        "Long pressed on " + car.make + " " + car.model,
+                        Toast.LENGTH_SHORT).show();
+
+                // todo edit selected vehicle
+                return true;
             }
         });
     }
