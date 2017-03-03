bc286be3e3feadb4a5d1237193ce0af783ff17f6
jarinyah <jarinyah@sfu.ca>
2017-03-03 12:35:32 -0800

    Remove extra code used for testing

--- app/src/main/java/sfu/cmpt276/carbontracker/MainActivity.java
+++ app/src/main/java/sfu/cmpt276/carbontracker/MainActivity.java
@@ -13,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent (MainActivity.this, MenuActivity.class); //remove once merged with welcome screen branch
-        startActivity(intent);
+
     }
 }
