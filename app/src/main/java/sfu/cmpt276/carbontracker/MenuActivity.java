e2e087a15a870c2cbb384a80f6bf15b8708ed7fd
xiangyil <xiangyi_li@sfu.ca>
2017-03-04 14:32:23 -0800

    welcome screen

---
+++ app/src/main/java/sfu/cmpt276/carbontracker/MenuActivity.java
@@ -0,0 +1,15 @@
+package sfu.cmpt276.carbontracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.apple.carbontracker.R;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_menu);
+        }
+    }
