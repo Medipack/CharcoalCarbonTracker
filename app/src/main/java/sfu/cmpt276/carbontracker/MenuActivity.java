27900189d90adf4483861834e3ede9c1ddef1773
xiangyil <xiangyil@sfu.ca>
2017-03-05 23:42:58 -0800

    Update MenuActivity.java

--- app/src/main/java/sfu/cmpt276/carbontracker/MenuActivity.java
+++ app/src/main/java/sfu/cmpt276/carbontracker/MenuActivity.java
@@ -8,7 +8,6 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.apple.carbontracker.R;
 import sfu.cmpt276.carbontracker.MenuActivity;
 
 public class MenuActivity extends AppCompatActivity {
