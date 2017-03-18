846ecf41be2ed04131330aa5ac48ac8260f47468
Calvin Chou <ckc21@sfu.ca>
2017-03-17 21:48:41 -0700

    made new editJourneyActivity and UI layout

---
+++ app/src/main/java/sfu/cmpt276/carbontracker/ui/EditJourneyActivity.java
@@ -0,0 +1,28 @@
+package sfu.cmpt276.carbontracker.ui;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import sfu.cmpt276.carbontracker.R;
+
+public class EditJourneyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_journey);
+
+        setupSelectModeTxt();
+
+
+
+    }
+
+    private void setupSelectModeTxt() {
+        TextView selectTxt = (TextView) findViewById(R.id.edit_journey_selectTransportationMode);
+        Typeface face = Typeface.createFromAsset(getAssets(),"fonts/Peter.ttf");
+        selectTxt.setTypeface(face);
+    }
+}
