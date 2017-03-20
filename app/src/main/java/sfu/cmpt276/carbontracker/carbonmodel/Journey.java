e377603e14ed0a8e1f1fc18b4f25152592f0378c
Calvin Chou <ckc21@sfu.ca>
2017-03-20 01:27:05 -0700

    Fixed bug where total distance is not indicated in JourneyEmissionsActivity. Added calculateTotalDistance helper function to Journey and made it so setRoute updates the attributes for the journey when called.

--- app/src/main/java/sfu/cmpt276/carbontracker/carbonmodel/Journey.java
+++ app/src/main/java/sfu/cmpt276/carbontracker/carbonmodel/Journey.java
@@ -24,8 +24,12 @@ public class Journey {
         car = new Car(); //initializes car as new car with default values
         route = new Route(); //initializes route as a new route with default values
         date = new Date(); //sets date to current date
-        totalDistance = route.getRouteDistanceCity() + route.getRouteDistanceHighway();
+        totalDistance = calculateTotalDistance();
         carbonEmitted = 0;
+    }
+
+    private double calculateTotalDistance() {
+        return route.getRouteDistanceCity() + route.getRouteDistanceHighway();
     }
 
     Journey(Car car, Route route) {
@@ -112,8 +116,11 @@ public class Journey {
         return route;
     }
 
+    //Add route and resets total distance and carbon emitted
     public void setRoute(Route route) {
         this.route = route;
+        totalDistance = calculateTotalDistance();
+        carbonEmitted = calculateCarbonEmission();
     }
 
 }
