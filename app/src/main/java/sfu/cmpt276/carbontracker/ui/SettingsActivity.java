54c93629241a2cdf3ffb0496908691cc196a93eb
Calvin Chou <ckc21@sfu.ca>
2017-03-28 17:37:39 -0700

    Added a settings activity for human relatable units

---
+++ app/src/main/java/sfu/cmpt276/carbontracker/ui/SettingsActivity.java
@@ -0,0 +1,15 @@
+package sfu.cmpt276.carbontracker.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sfu.cmpt276.carbontracker.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
