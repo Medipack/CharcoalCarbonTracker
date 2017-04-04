54073ec4032046b15e55564475854e016f16bfb4
Tim Robertson <timr@sfu.ca>
2017-04-03 22:48:07 -0700

    Notification is displayed after 10 sec - Tapping correctly goes to Add Journey Activity

--- app/src/main/java/sfu/cmpt276/carbontracker/ui/NotificationThread.java
+++ app/src/main/java/sfu/cmpt276/carbontracker/ui/NotificationThread.java
@@ -1,24 +1,31 @@
 package sfu.cmpt276.carbontracker.ui;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import java.lang.ref.WeakReference;
 
-import sfu.cmpt276.carbontracker.carbonmodel.User;
+import sfu.cmpt276.carbontracker.R;
 
-import static android.support.v4.content.ContextCompat.startActivity;
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 /**
  * Thread for handling Notifications
  * Waits until specified time to show notification to user
  */
 public class NotificationThread extends Thread {
+
+    private static final int ADD_JOURNEY_NOTIFICATION_CODE = 1;
+    private static final int ADD_BILL_NOTIFICATION_CODE = 2;
+    private static final int NOTIFICATION_ICON = R.mipmap.car1;
 
     // Thanks http://www.androiddesignpatterns.com/2013/01/inner-class-handler-memory-leak.html
     private static class NotificationHandler extends Handler {
@@ -62,25 +69,45 @@ public class NotificationThread extends Thread {
         // no journeys entered today? notify
         // only 1 journey entered today? notify
         // no bills in last 1.5 months? notify
+        Intent intent = getNewJourneyIntent(context);
 
-        displayAddJourneyNotification();
+        PendingIntent pendingIntent = intentToPendingIntent(intent, context);
+
+        Notification notification = buildNotification("Add a Journey for Today",
+                "You haven't added any Journey's today. Tap to add!",
+                NOTIFICATION_ICON,
+                pendingIntent,
+                context);
+
+        NotificationManager manager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+
+        manager.notify(ADD_JOURNEY_NOTIFICATION_CODE, notification);
     }
 
+    private Notification buildNotification(String title, String content, int iconId,
+                                           PendingIntent pendingIntent, Context context) {
+        Notification notification = new NotificationCompat.Builder(context)
+                .setSmallIcon(iconId)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setContentIntent(pendingIntent).build();
 
-    private void displayAddJourneyNotification() {
-        // todo notification
-        startNewJourney();
+        notification.flags |= Notification.FLAG_AUTO_CANCEL;
+
+        return notification;
     }
 
-    private void displayAddBillNotification() {
-        // todo
+    private PendingIntent intentToPendingIntent(Intent intent, Context context) {
+        return PendingIntent.getActivity(
+                        context,
+                        0,
+                        intent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
     }
 
-    private void startNewJourney() {
-        User.getInstance().createNewCurrentJourney();
-        Intent intent = new Intent(context, TransportationModeActivity.class);
-        Bundle emptyBundle = new Bundle();
-        startActivity(context, intent, emptyBundle);
+    private Intent getNewJourneyIntent(Context context) {
+        return new Intent(context, TransportationModeActivity.class);
     }
 
     public Handler getNotificationhandler() {
